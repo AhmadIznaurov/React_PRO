ddbe8415d8bda0e7745dcb165588d0680cf33157
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-07-04 16:38:50 +0300

    second

---
+++ src/Lessons/Lesson2/Fetch.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export const Fetch = () => {
+
+    // const promise = new Promise((resolve, reject) => {
+    //     const a = 700;
+    //     const b = 500;
+    //     const c = a > b
+    //
+    //     if (c) {
+    //         resolve(console.log(`This component is ${c}`));
+    //     } else {
+    //         reject('ERROR')
+    //     }
+    // })
+    //
+    // promise.then((res) =>  {
+    //     return console.log(res);
+    // }).catch((error) => {
+    //     console.log(error);
+    // })
+
+
+
+
+    // const getUsers = () => {
+    //     return fetch("https://jsonplaceholder.typicode.com/users")
+    //     .then((response) => {
+    //         if (!response.ok) {
+    //             throw new Error('Not answer from the server');
+    //         }
+    //         return response.json();
+    //     })
+    // }
+    // getUsers()
+    //     .then((users) => {
+    //         console.log(users);
+    //     }).catch((e) => {
+    //     console.log(e);
+    // })
+
+
+
+    
+
+}
